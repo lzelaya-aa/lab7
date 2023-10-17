f1fac6c9e7be74deb9e5c993bd914fec10121688
lzelaya-aa <litzyzelayaa@gmail.com>
2023-10-16 21:29:37 -0500

    updated js file

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,32 +1,45 @@
-// lab8.js
-
 function getDataFromForm() {
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  
+  // Obtain the first name and last name from the form elements
+  var firstName = document.getElementsByName("fname")[0].value;
+  var lastName = document.getElementsByName("lname")[0].value;
+
+  // Call runAjax and send the two strings as arguments
   runAjax(firstName, lastName);
 }
 
 function runAjax(fname, lname) {
-  const xhr = new XMLHttpRequest();
-  const url = "./ajax.php";
-  const params = "firstName=" + fname + "&lastName=" + lname;
+  // Create an XMLHttpRequest object
+  var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", url + "?" + params, true);
+  // Define the URL for the AJAX request
+  var url = "./ajax.php";
 
+  // Add the first name and last name as query parameters
+  url += "?fname=" + fname + "&lname=" + lname;
+
+  // Open a GET request to the specified URL
+  xhr.open("GET", url, true);
+
+  // Set up the event handler for when the request is complete
   xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          const response = xhr.responseText;
-          if (response.charAt(0) === '#') {
-              // Error occurred
-              const errorNumber = response.substring(1);
-              alert("Error " + errorNumber + " Occurred");
-          } else {
-              // Set response text to the paragraph element
-              document.getElementById("responseString").textContent = response;
-          }
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        // Check if the response is a string
+        if (typeof xhr.responseText === 'string') {
+          // Change the text of the paragraph element with the id "responseString"
+          document.getElementById("responseString").textContent = xhr.responseText;
+        } else {
+          // Handle the case where the response is not a string
+          alert("Error " + xhr.status + " Occurred");
+        }
+      } else {
+        // Handle errors, e.g., when the request fails
+        alert("Error " + xhr.status + " Occurred");
       }
+    }
   };
 
+  // Send the AJAX request
   xhr.send();
 }
+
