a46b154d77caa51ae7a4e052c4a88bfcc5db70c2
lzelaya-aa <litzyzelayaa@gmail.com>
2023-10-17 14:12:38 -0500

    delete

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -9,31 +9,6 @@ function getDataFromForm() {
     runAjax(firstName, lastName);
 }
 
-// Function to make an AJAX request
-function runAjax(fname, lname) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "./ajax.php?fname=" + fname + "&lname=" + lname, true);
-
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-              var response = xhr.responseText;
-              if (response.indexOf("Error") === 0) {
-                  // Handle the error case
-                  var errorNumber = response.match(/\d+/);
-                  alert("Error " + errorNumber + " Occurred");
-              } else {
-                  // Update the responseString paragraph with the response
-                  document.getElementById("responseString").textContent = response;
-              }
-          } else {
-              // Handle other HTTP status codes
-              alert("Error " + xhr.status + " Occurred");
-          }
-      }
-  };
-
-  xhr.send();
-}
+//
 
 
