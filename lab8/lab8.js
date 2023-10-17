c362bc2e0c3d7d8cfa3a31d92af3e15aa38f6b54
lzelaya-aa <litzyzelayaa@gmail.com>
2023-10-17 14:08:31 -0500

    console log

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -4,6 +4,8 @@ function getDataFromForm() {
   // Obtain the first name and last name from the form elements
     var firstName = document.querySelector('input[name="fname"]').value;
     var lastName = document.querySelector('input[name="lname"]').value;
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
     runAjax(firstName, lastName);
 }
 
