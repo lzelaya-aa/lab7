c7163695ea86e0a1b15123db8d178ffcfea63c6a
lzelaya-aa <litzyzelayaa@gmail.com>
2023-10-17 14:48:41 -0500

    lab8 folder

--- lab8/ajax.php
+++ lab8/ajax.php
@@ -1,4 +1,4 @@
 <?php
 
-//echo "Hello ".$_GET[‘fname’]." ".$_GET[‘lname’]."!";
-echo "Hello ".$_GET[fname]." ".$_GET[lname]."!";
+echo "Hello ".$_GET[‘fname’]." ".$_GET[‘lname’]."!";
+//echo "Hello ".$_GET[fname]." ".$_GET[lname]."!";
