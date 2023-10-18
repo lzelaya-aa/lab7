6717fd6ea011f018048b0618c75a73bdd8dd28c7
lzelaya-aa <litzyzelayaa@gmail.com>
2023-10-17 19:23:18 -0500

    cc

--- lab8/ajax.php
+++ lab8/ajax.php
@@ -1,5 +1,14 @@
 <?php
+if (isset($_GET['fname']) && isset($_GET['lname'])) {
+    $firstName = $_GET['fname'];
+    $lastName = $_GET['lname'];
 
-echo "Hello ".$_GET[‘fname’]." ".$_GET[‘lname’]."!";
-//echo "Hello ".$_GET[fname]." ".$_GET[lname]."!";
+    // You can perform any processing you need here, for example:
+    $response = "Hello " . $firstName . " " . $lastName . "!";
+    
+    echo $response;
+} else {
+    // Handle the case where the parameters are missing
+    echo "Error: Missing first name or last name.";
+}
 ?>
