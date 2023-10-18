10ffc554b2aec32550c05f467df33c296f8de7a2
lzelaya-aa <litzyzelayaa@gmail.com>
2023-10-17 19:14:25 -0500

    cc

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -12,19 +12,13 @@ function runAjax(fname, lname) {
   var xhr = new XMLHttpRequest();
 
   // Define the AJAX request
-  xhr.open("GET", "ajax.php?fname=" + fname + "&lname=" + lname, true);
+  xhr.open("GET", "test.php?fname=" + fname + "&lname=" + lname, true);
 
   // Set up a callback function to handle the response
   xhr.onload = function () {
     if (xhr.status === 200) {
-      // Check if the response starts with "Hello" (as in your original PHP response)
-      if (xhr.responseText.startsWith("Hello")) {
-        // If it's a valid response, change the text of the paragraph element
-        document.getElementById("responseString").textContent = xhr.responseText;
-      } else {
-        // If there's an unexpected response, alert the user
-        alert("Unexpected response: " + xhr.responseText);
-      }
+      // If the response is a string, change the text of the paragraph element
+      document.getElementById("responseString").textContent = xhr.responseText;
     } else {
       // If there is an error, alert the user
       alert("Error " + xhr.status + " Occurred");
@@ -34,6 +28,7 @@ function runAjax(fname, lname) {
   // Send the AJAX request
   xhr.send();
 }
+
 
 
 
