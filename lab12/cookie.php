fac5f19d32f6fbc4633d4a5a0ac851f654ad2266
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-10 11:36:53 -0600

    addded cookie.php for lab 12

---
+++ lab12/cookie.php
@@ -0,0 +1,20 @@
+<?php
+// Name of the cookie
+$cookieName = "cs4413";
+
+// Check if the cookie is already set
+if (isset($_COOKIE[$cookieName])) {
+    // Cookie is already set
+    $cookieValue = $_COOKIE[$cookieName];
+    echo "Cookie '$cookieName' is already set with the value: $cookieValue";
+} else {
+    // Cookie is not set, so we'll set it
+    $cookieValue = "YourCookieValue"; // Set your desired value here
+    $expirationTime = time() + 3600;  // Cookie will expire in 1 hour (adjust as needed)
+
+    // Set the cookie
+    setcookie($cookieName, $cookieValue, $expirationTime, '/');
+
+    echo "Cookie '$cookieName' is now set with the value: $cookieValue";
+}
+?>
