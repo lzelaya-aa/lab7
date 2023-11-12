4810d67180c620b16586162732eeb0e37c1962f2
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-12 12:02:31 -0600

    cc

--- lab12/cookie.php
+++ lab12/cookie.php
@@ -6,16 +6,16 @@ $cookieName = "cs4413";
 if (isset($_COOKIE[$cookieName])) {
     // Cookie is already set
     $cookieValue = $_COOKIE[$cookieName];
-    echo "Cookie '$cookieName' is already set with the value: $cookieValue";
+    echo "Cookie '$cookieName' is already set with the value: $cookieValue with expiration time of: $expirationTime";
 } else {
     // Cookie is not set, so we'll set it
     $cookieValue = "WebTechCookie"; // Set your desired value here
-    $expirationTime = time() + 3600;  // Cookie will expire in 1 hour (adjust as needed)
+    $expirationTime = time() + -3600;  // Cookie will expire in 1 hour (adjust as needed)
 
     // Set the cookie
     setcookie($cookieName, $cookieValue, $expirationTime, '/');
 
-    echo "Cookie '$cookieName' is now set with the value: $cookieValue";
+    echo "Cookie '$cookieName' is now set with the value: $cookieValue with expiration time of: $expirationTime";
 }
 ?>
 
